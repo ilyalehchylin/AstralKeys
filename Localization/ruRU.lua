ae83eb242d9bd9b4024538433ebb7734f00a3b56
Ilya Lehchylin <lehchylin.ilya@gmail.com>
2020-08-30 13:13:36 +0300

    [ADD] Russian localization

--- Localization/ruRU.lua
+++ Localization/ruRU.lua
@@ -12,15 +12,15 @@ L['DUNGEON'] = DUNGEONS
 L['CHARACTER'] = CHARACTER
 
 -- Subsection Headers
-L['CHARACTERS'] = 'CHARACTERS'
-L['AFFIXES'] = 'AFFIXES'
+L['CHARACTERS'] = 'ПЕРСОНАЖИ'
+L['AFFIXES'] = 'МОДИФИКАТОРЫ'
 
 -- Character Labels
-L['CURRENT_KEY'] = 'CURRENT'
-L['WEEKLY_BEST'] = 'WKLY BEST'
+L['CURRENT_KEY'] = 'ТЕКУЩИЙ'
+L['WEEKLY_BEST'] = 'ЛУЧШ. НЕД.'
 
-L['CHARACTER_DUNGEON_NOT_RAN'] = 'No mythic+ ran'
-L['CHARACTER_KEY_NOT_FOUND'] = 'No key found'
+L['CHARACTER_DUNGEON_NOT_RAN'] = 'Нет прохождения'
+L['CHARACTER_KEY_NOT_FOUND'] = 'Ключ не найден'
 
 
 -- Dropdown menu selections
@@ -31,15 +31,28 @@ L['REQUEST_INVITE'] = REQUEST_INVITE
 L['CANCEL'] = CANCEL
 
 -- Announce messages
-L['ANNOUNCE_NEW_KEY'] = 'Astral Keys: New key %s'
-L['NO_KEY'] = 'No key'
-L['KEYS_RESPOND_ON_NO_KEY'] = 'Respond even if you do not have a key'
+L['ANNOUNCE_NEW_KEY'] = 'Astral Keys: Новый ключ %s'
+L['NO_KEY'] = 'Нет ключа'
+L['KEYS_RESPOND_ON_NO_KEY'] = 'Отправлять сообщение, даже если у вас нет ключа'
 
 
 -- Search field texts
-L['FILTER_TEXT_DUNGEON'] = 'Filter by dungeon'
-L['FILTER_TEXT_CHARACTER'] = 'Filter by character name'
+L['FILTER_TEXT_DUNGEON'] = 'Поиск по подземелью'
+L['FILTER_TEXT_CHARACTER'] = 'Поиск по персонажу'
 
 -- Options
-L['!KEYS_DESC'] = 'Respond to !keys command in the following chat channels'
-L['EXPANDED_TOOLTIP'] = 'Show affix description in the tooltip'
+L['GENERAL_OPTIONS'] = 'ОБЩИЕ НАСТРОЙКИ'
+L['SHOW_OFFLINE_PLAYERS'] = 'Показывать отсутствующих\nигроков'
+L['SHOW_MINIMAP_BUTTON'] = 'Показывать кнопку у миникарты'
+L['SHOW_CURRENT_KEY_TOOLTIP'] = 'Показывать текущий ключ\nв подсказке'
+L['DISPLAY_OFFLINE_BELOW_ONLINE'] = 'Показывать игроков не в сети ниже находящихся в игре'
+L['ANNOUNCE_NEW_KEYS_PARTY'] = 'Объявлять группе о новых\nключах'
+L['ANNOUNCE_NEW_KEYS_GUILD'] = 'Объявлять гильдии о новых ключах'
+L['RANK_FILTER'] = 'Фильтр Званий'
+L['INCLUDE_RANKS_GUILD_LISTING'] = 'Включить эти звания в список гильдии'
+L['SHOW_OTHER_FACTION'] = 'Показывать другую фракцию'
+L['SYNC_FRIENDS'] = 'Синхронизировать с друзьями'
+L['SYNC_OPTIONS'] = 'НАСТРОЙКИ СИНХРОНИЗАЦИИ'
+L['!KEYS_CHAT_COMMAND'] = 'Команда для чата !keys'
+L['!KEYS_DESC'] = 'Отвечать на команду !keys в следующих каналах чата'
+L['EXPANDED_TOOLTIP'] = 'Показывать описание модификатора в подсказке'
